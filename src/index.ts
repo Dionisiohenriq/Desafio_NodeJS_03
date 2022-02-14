41afb544ebc55776c134701ca60d74f8b0acc1f5
dionisiohenriq <henrique.contractor@am.jll.com>
2022-02-14 01:10:12 -0300

    Desafio 3 concluído

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * Required External Modules
  */
-
+const prompt = require('prompt-sync')();
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -36,8 +36,46 @@ app.use(express.json());
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
+	// 		REQUERIMENTOS:
+	// R01 – IMPRIMIR MENSAGEM PERGUNTANDO QUANTIDADE DE
+	// ALUNOS;
+	// R02 – LER E ARMAZENAR INFORMAÇÃO;
+	// R03 – IMPRIMIR MENSAGEM PERGUNTANDO NOME E NOTA DO
+	// ALUNO (DE ACORDO COM A QUANTIDADE DO R01);
+	// R04 – IMPRIMIR O ALUNO COM MAIOR NOTA;
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
 	
+	// R01 – IMPRIMIR MENSAGEM PERGUNTANDO QUANTIDADE DE ALUNOS; 
+	// R02 - LER E ARMAZENAR INFORMAÇÃO;
+	let alunosQtd = prompt("Insira a qtd de alunos\n");
+
+	// R03 - IMPRIMIR MENSAGEM PERGUNTANDO NOME E NOTA DO
+	// ALUNO (DE ACORDO COM A QUANTIDADE DO R01);
+	let alunos = [];
+
+	for(let i = 0; i <= alunosQtd.length; i++){
+		let obj = Object.create(null);
+		obj.Nome = prompt(`Qual o nome do ${i+1}º aluno?`);
+		obj.Nota = parseFloat(prompt(`Qual a nota do ${i+1}º aluno?`));
+		alunos.push(obj);
+	}
+	alunos.forEach(aluno => console.log(aluno));
+
+	// R04 – IMPRIMIR O ALUNO COM MAIOR NOTA;
+	let obj = Object.create(null);
+	obj.Nome = "";
+	obj.Nota = 0.0;
+
+	for(let i = 0; i < alunos.length; i++){
+		if(obj.Nota < alunos[i].Nota){
+			obj.Nome = alunos[i].Nome;
+			obj.Nota = alunos[i].Nota;
+		}
+	}
+	
+	console.log(`Aluno com a maior nota: \n${obj.Nome} - Nota: ${obj.Nota}`);
+
+
 });
